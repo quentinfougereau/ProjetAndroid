0cb75441e5b626f51d5b9e20971a063a7b96874e
vartankoko <vartan.koko@gmail.com>
2017-02-06 20:06:28 +0100

    correction espace vide si il n'y a pas d'image

--- Agnoscere/app/src/main/java/com/esgi/agnoscere/AnecdoteActivity2.java
+++ Agnoscere/app/src/main/java/com/esgi/agnoscere/AnecdoteActivity2.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -61,6 +62,12 @@ public class AnecdoteActivity2 extends AppCompatActivity implements View.OnClick
         mEditText.setOnClickListener(this);
         sendComment.setOnClickListener(this);
 
+        if(mAnecdote.getImagelink().length() == 0) {
+            ImageView imageView = (ImageView) findViewById(R.id.image_view);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) imageView.getLayoutParams();
+            params.height = 0;
+            imageView.setLayoutParams(params);
+        }
 
         setCommentList();
         setAnecdoteTextView();
